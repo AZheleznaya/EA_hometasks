14108c790ba082185f80d100314845cac87624b1
AZheleznaya <aanastasiyazheleznaya@gmail.com>
2023-03-30 16:59:06 +0300

    bootstrap task: header task

---
+++ 02_Module/01_Day_bootstrap/scripts/main.js
@@ -0,0 +1,36 @@
+const updateSize = () => {
+    let vh = window.innerHeight * 0.01;
+    let vw = document.documentElement.clientWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+
+window.addEventListener("resize", updateSize);
+
+window.addEventListener("load", () => {
+    updateSize();
+
+    document.querySelector(".preloader").classList.add("hidden");
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector(".header__desktop");
+    const closeBackground = document.querySelector(".header__close");
+    const menuButton = document.querySelector(".mobile-menu");
+    const closeMenuCross = document.querySelector(".header__close-icon");
+
+    if(menuButton) {
+        menuButton.addEventListener("click", () => {
+            closeBackground.classList.add("active");
+            menu.classList.add("active");
+
+        })
+    }
+
+    if(closeMenuCross) {
+        closeMenuCross.addEventListener("click", () => {
+            closeBackground.classList.remove("active");
+            menu.classList.remove("active");
+        })
+    }
+})
