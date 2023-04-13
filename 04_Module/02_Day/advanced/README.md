dba1fb11cb48eb71f6a394292f864aa6cfe36b84
AZheleznaya <aanastasiyazheleznaya@gmail.com>
2023-04-13 23:01:14 +0300

    Module 4

--- 04_Module/02_Day/advanced/README.md
+++
@@ -1,9 +0,0 @@
-# Farison Lawn Care
-
-## Getting started
-
-```
-npm install
-npm run start -- --devUrl https://your-dev-url.local
-npm run build
-```
--- 04_Module/02_Day/advanced/resources/js/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-	setTimeout(() => {
-		window.dispatchEvent(new Event("touchend"));
-		window.dispatchEvent(new Event("click"));
-	}, 2500);
-});
